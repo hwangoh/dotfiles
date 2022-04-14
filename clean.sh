bce9be5fbeeb4677f7a0b4a73a5aa0d929a43c54
hwangoh <hwan.goh@macso.ai>
2022-04-15 10:10:59 +1200

    added install and clean bash scripts

---
+++ clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -rf Workflow_Notes
+rm -rf Images
+rm -rf i3wm-windows-emulator
+rm -rf inkdrop
+rm -rf LaTeX_Templates
+rm -rf vim_sessions
+rm -rf tmux_sessions
+rm -rf i3_config_laptop
+rm -rf i3_config_work
+rm -rf i3_exit.sh
+rm -rf i3_status.conf
+rm -rf tmux.conf
+rm -rf symlinks.sh
+rm -rf bash_aliases
+rm -rf bashrc
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install build-essential
+sudo apt install make
+sudo apt-get -y install cmake
+sudo apt-get install python-dev-is-python3
