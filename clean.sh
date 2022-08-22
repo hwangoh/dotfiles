e31dbbb04952f9f9a2c76fb3aaeb6eb19755474d
hwangoh <hwan.goh@macso.ai>
2022-08-23 09:16:21 +1200

    updated clean.sh

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 rm -rf config_files
-rm -rf Workflow_Notes
-rm -rf Images
-rm -rf LaTeX_Templates
+rm -rf workflow_notes
+rm -rf images
+rm -rf latex_templates
 rm -rf symlinks.sh
 rm -rf vim_sessions
 rm -rf tmux_sessions
