3940df76948f4d7d0fea6fcf0a045e8cfc31ca07
macso-hwangoh <hwan.goh@macso.ai>
2025-04-22 17:16:22 +1200

    updated new instructions for forward backwards search

--- install_vimtex_packages.sh
+++ install_vimtex_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get -y install zathura
-sudo apt-get -y install vim-gtk
+sudo apt-get -y install vim-gtk3
 sudo apt-get -y install xdotool
 sudo apt-get -y install texlive-full
